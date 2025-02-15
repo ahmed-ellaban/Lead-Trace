380d52e299864dcd38568fbc956bbade77cae64f
Ahmed Ellaban <ahmedsamyallban2000@gmail.com>
2025-02-15 18:40:13 +0200

    new platform new files

---
+++ AI-ML.py
@@ -0,0 +1,27 @@
+import os
+from openai import OpenAI
+
+client = OpenAI(
+    base_url="https://api.aimlapi.com/v1",
+
+    # Insert your AIML API Key in the quotation marks instead of <YOUR_API_KEY>.
+    api_key="<YOUR_API_KEY>",
+)
+
+response = client.chat.completions.create(
+    model="gpt-4o",
+    messages=[
+        {
+            "role": "system",
+            "content": "You are an AI assistant who knows everything.",
+        },
+        {
+            "role": "user",
+            "content": "Tell me, why is the sky blue?"
+        },
+    ],
+)
+
+message = response.choices[0].message.content
+
+print(f"Assistant: {message}")
